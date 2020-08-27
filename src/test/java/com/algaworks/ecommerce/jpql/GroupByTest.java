c9db30796f7c5402b522c2c497c08383360899f8
Samuel Dutra <samuel.dutra@hotmail.com>
2020-08-26 21:21:52 -0300

    Exercício de group by feito com sucesso

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -14,15 +14,41 @@ public class GroupByTest extends EntityManagerTest {
     @Test
     public void agruparResultado(){
         // Quantidade de produtos por categoria.
-        // Total de vendas por mês.
-        // Total de vandas por categoria.
+//        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
 
-        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+        // Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/',function('monthname', p.dataCriacao)), sum(p.total) from Pedido p " +
+//                "group by year(p.dataCriacao), month(p.dataCriacao)";
+
+        // Total de vandas por categoria.
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id";
+
+        // Total de vendas por cliente
+//        String jpql = "select cli.nome, sum(i.precoProduto) from Cliente cli join cli.pedidos ped join ped.itens i group by cli.id"; // Minha solução
+
+        // Solução professor
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+
+        // Total de vendas por dia e por categoria
+        String jpql = "select concat(cat.nome, ' ', date(ped.dataCriacao)), sum(ip.precoProduto) from Pedido ped join ped.itens ip join ip.produto pro join pro.categorias cat group by cat.id, date(ped.dataCriacao)"; // Minha solução
+
+        // Solução professor
+//        String jpql = "select " +
+//                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+//                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+//                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+//                " order by p.dataCriacao, c.nome ";
+
+
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
         List<Object[]> lista = typedQuery.getResultList();
-//        Assert.assertFalse(lista.isEmpty());
+        Assert.assertFalse(lista.isEmpty());
 
         lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1]));
     }
